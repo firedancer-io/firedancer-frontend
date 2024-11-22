74114b171c098efda80608c9c76fd0a70c40a551
atsai-jump <atsai@jumptrading.com>
2024-11-22 11:08:23 -0600

    remove unused files

--- src/features/Gossip/atoms.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "jotai";
-import { SortOptions } from "./types";
-
-export const gossipSearchAtom = atom("");
-
-export const gossipSortAtom = atom<SortOptions>(SortOptions.StakeWeightDsc);
--- src/features/Gossip/types.ts
+++
@@ -1,4 +0,0 @@
-export enum SortOptions {
-  StakeWeightDsc = "Stake Weight (High to Low)",
-  StakeWeightAsc = "Stake Weight (Low to High)",
-}
