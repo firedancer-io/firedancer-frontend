293b1240875975bbb3d557029fe28e7765175168
Ami Suzuki <asuzuki@jumptrading.com>
2025-08-05 18:05:39 -0500

    chore: update slot progression chart start line color

--- src/features/Overview/SlotPerformance/ComputeUnitsCard/CuChartInfoIcon.tsx
+++ src/features/Overview/SlotPerformance/ComputeUnitsCard/CuChartInfoIcon.tsx
@@ -1,6 +1,7 @@
 import { InfoCircledIcon } from "@radix-ui/react-icons";
 import { Tooltip } from "@radix-ui/themes";
 import styles from "./cuChartIcon.module.css";
+import { startLineColor } from "../../../../colors";
 
 export const infoIconId = "cu-chart-info-icon";
 
@@ -10,7 +11,11 @@ export default function CuChartInfoIcon() {
   return (
     <div id={infoIconId} className={styles.iconContainer}>
       <Tooltip content="The 'revenue' scheduler strategy waits until 50ms remain in the block to schedule non-bundle user transactions">
-        <InfoCircledIcon width={iconSize} height={iconSize} />
+        <InfoCircledIcon
+          width={iconSize}
+          height={iconSize}
+          color={startLineColor}
+        />
       </Tooltip>
     </div>
   );
