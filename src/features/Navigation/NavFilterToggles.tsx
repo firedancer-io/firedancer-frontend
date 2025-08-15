53a06c097f9dd9fbd868b1064aab8c8b085aeec3
Ami Suzuki <asuzuki@jumptrading.com>
2025-08-15 09:20:44 -0500

    feat: slot navigation filter toggles

---
+++ src/features/Navigation/NavFilterToggles.tsx
@@ -0,0 +1,52 @@
+import { Flex, Text } from "@radix-ui/themes";
+import { ToggleGroup } from "radix-ui";
+import { useCallback } from "react";
+
+import { useAtom, useSetAtom } from "jotai";
+import {
+  SlotNavFilter,
+  slotNavFilterAtom,
+  slotOverrideAtom,
+} from "../../atoms";
+import styles from "./navigation.module.css";
+
+export default function NavFilterToggles() {
+  const [navFilter, setNavFilter] = useAtom(slotNavFilterAtom);
+  const setSlotOverride = useSetAtom(slotOverrideAtom);
+
+  const onValueChange = useCallback(
+    (value: SlotNavFilter) => {
+      if (!value) return;
+
+      setSlotOverride(undefined);
+      setNavFilter(value);
+    },
+    [setNavFilter, setSlotOverride],
+  );
+
+  return (
+    <Flex width="100%">
+      <ToggleGroup.Root
+        type="single"
+        value={navFilter}
+        aria-label="Slots List Toggle"
+        onValueChange={onValueChange}
+        className={styles.navFilterToggleGroup}
+      >
+        <ToggleGroup.Item
+          value={SlotNavFilter.AllSlots}
+          aria-label="All Slots toggle"
+        >
+          <Text>All Slots</Text>
+        </ToggleGroup.Item>
+
+        <ToggleGroup.Item
+          value={SlotNavFilter.MySlots}
+          aria-label="My Slots toggle"
+        >
+          <Text>My Slots</Text>
+        </ToggleGroup.Item>
+      </ToggleGroup.Root>
+    </Flex>
+  );
+}
