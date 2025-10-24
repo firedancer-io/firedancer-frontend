30c0488095db86b349c1a2579144684119ff7322
Ami Suzuki <asuzuki@jumptrading.com>
2025-10-24 14:12:36 -0500

    fix: handle unchanging cumulative value for rate calculation

--- src/features/StartupProgress/Firedancer/useValuePerSecond.ts
+++ src/features/StartupProgress/Firedancer/useValuePerSecond.ts
@@ -1,26 +1,56 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
+import { useInterval } from "react-use";
+
+/**
+ * Append new value, and delete stale values.
+ * Keep at least 2 values to continue supporting rate calc
+ */
+function addNewValueAndShift(
+  prev: [number, number][],
+  newValue: number,
+  now: number,
+  windowMs: number,
+) {
+  const newValues = [...prev, [newValue, now]] satisfies [number, number][];
+
+  while (
+    // keep at least two for rate calc
+    newValues.length > 2 &&
+    newValues[0] &&
+    newValues[0][1] <= now - windowMs
+  ) {
+    newValues.shift();
+  }
+  return newValues;
+}
 
 export function useValuePerSecond(
   cumulativeValue?: number | null,
   windowMs = 500,
 ) {
+  // array of [cumulative value, time]
   const [values, setValues] = useState<[number, number][]>([]);
 
   useEffect(() => {
     if (cumulativeValue == null) return;
 
     const now = performance.now();
-    setValues((prev) => {
-      const newValues = [...prev, [cumulativeValue, now]] satisfies [
-        number,
-        number,
-      ][];
-      while (newValues[0] && newValues[0][1] <= now - windowMs) {
-        newValues.shift();
-      }
-      return newValues;
-    });
+    setValues((prev) =>
+      addNewValueAndShift(prev, cumulativeValue, now, windowMs),
+    );
   }, [cumulativeValue, windowMs]);
+
+  // Handle unchanged cumulative values by
+  // adding the unchanged value to the end of the array
+  useInterval(() => {
+    const now = performance.now();
+    const latestValue = values[values.length - 1];
+    if (latestValue && latestValue[1] < now - windowMs) {
+      setValues((prev) =>
+        addNewValueAndShift(prev, latestValue[0], now, windowMs),
+      );
+    }
+  }, windowMs);
 
   const reset = useCallback(() => {
     setValues([]);
