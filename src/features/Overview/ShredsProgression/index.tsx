06044386a1da22c80d74764d3c60d562f57f95e2
Ami Suzuki <asuzuki@jumptrading.com>
2025-11-04 10:39:27 -0600

    fix: only show shreds progression for firedancer

--- src/features/Overview/ShredsProgression/index.tsx
+++ src/features/Overview/ShredsProgression/index.tsx
@@ -2,8 +2,15 @@ import { Flex } from "@radix-ui/themes";
 import Card from "../../../components/Card";
 import CardHeader from "../../../components/CardHeader";
 import ShredsTiles from "./ShredsTiles";
+import { useAtomValue } from "jotai";
+import { ClientEnum } from "../../../api/entities";
+import { clientAtom } from "../../../atoms";
 
 export default function ShredsProgression() {
+  const client = useAtomValue(clientAtom);
+
+  if (client !== ClientEnum.Firedancer) return;
+
   return (
     <Card>
       <Flex direction="column" height="100%" gap="2">
