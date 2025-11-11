e16342ddc7c521c155696b8181cae23b6050967c
Ami Suzuki <asuzuki@jumptrading.com>
2025-11-11 09:07:54 -0600

    fix: route tree file

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,137 +8,121 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-// Import Routes
+import { Route as rootRouteImport } from './routes/__root'
+import { Route as SlotDetailsRouteImport } from './routes/slotDetails'
+import { Route as LeaderScheduleRouteImport } from './routes/leaderSchedule'
+import { Route as GossipRouteImport } from './routes/gossip'
+import { Route as AboutRouteImport } from './routes/about'
+import { Route as IndexRouteImport } from './routes/index'
 
-import { Route as rootRoute } from "./routes/__root";
-import { Route as SlotDetailsImport } from "./routes/slotDetails";
-import { Route as LeaderScheduleImport } from "./routes/leaderSchedule";
-import { Route as GossipImport } from "./routes/gossip";
-import { Route as AboutImport } from "./routes/about";
-import { Route as IndexImport } from "./routes/index";
-
-// Create/Update Routes
-
-const SlotDetailsRoute = SlotDetailsImport.update({
-  id: "/slotDetails",
-  path: "/slotDetails",
-  getParentRoute: () => rootRoute,
-} as any);
-
-const LeaderScheduleRoute = LeaderScheduleImport.update({
-  id: "/leaderSchedule",
-  path: "/leaderSchedule",
-  getParentRoute: () => rootRoute,
-} as any);
-
-const GossipRoute = GossipImport.update({
-  id: "/gossip",
-  path: "/gossip",
-  getParentRoute: () => rootRoute,
-} as any);
-
-const AboutRoute = AboutImport.update({
-  id: "/about",
-  path: "/about",
-  getParentRoute: () => rootRoute,
-} as any);
-
-const IndexRoute = IndexImport.update({
-  id: "/",
-  path: "/",
-  getParentRoute: () => rootRoute,
-} as any);
-
-// Populate the FileRoutesByPath interface
-
-declare module "@tanstack/react-router" {
-  interface FileRoutesByPath {
-    "/": {
-      id: "/";
-      path: "/";
-      fullPath: "/";
-      preLoaderRoute: typeof IndexImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/about": {
-      id: "/about";
-      path: "/about";
-      fullPath: "/about";
-      preLoaderRoute: typeof AboutImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/gossip": {
-      id: "/gossip";
-      path: "/gossip";
-      fullPath: "/gossip";
-      preLoaderRoute: typeof GossipImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/leaderSchedule": {
-      id: "/leaderSchedule";
-      path: "/leaderSchedule";
-      fullPath: "/leaderSchedule";
-      preLoaderRoute: typeof LeaderScheduleImport;
-      parentRoute: typeof rootRoute;
-    };
-    "/slotDetails": {
-      id: "/slotDetails";
-      path: "/slotDetails";
-      fullPath: "/slotDetails";
-      preLoaderRoute: typeof SlotDetailsImport;
-      parentRoute: typeof rootRoute;
-    };
-  }
-}
-
-// Create and export the route tree
+const SlotDetailsRoute = SlotDetailsRouteImport.update({
+  id: '/slotDetails',
+  path: '/slotDetails',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const LeaderScheduleRoute = LeaderScheduleRouteImport.update({
+  id: '/leaderSchedule',
+  path: '/leaderSchedule',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const GossipRoute = GossipRouteImport.update({
+  id: '/gossip',
+  path: '/gossip',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const AboutRoute = AboutRouteImport.update({
+  id: '/about',
+  path: '/about',
+  getParentRoute: () => rootRouteImport,
+} as any)
+const IndexRoute = IndexRouteImport.update({
+  id: '/',
+  path: '/',
+  getParentRoute: () => rootRouteImport,
+} as any)
 
 export interface FileRoutesByFullPath {
-  "/": typeof IndexRoute;
-  "/about": typeof AboutRoute;
-  "/gossip": typeof GossipRoute;
-  "/leaderSchedule": typeof LeaderScheduleRoute;
-  "/slotDetails": typeof SlotDetailsRoute;
+  '/': typeof IndexRoute
+  '/about': typeof AboutRoute
+  '/gossip': typeof GossipRoute
+  '/leaderSchedule': typeof LeaderScheduleRoute
+  '/slotDetails': typeof SlotDetailsRoute
 }
-
 export interface FileRoutesByTo {
-  "/": typeof IndexRoute;
-  "/about": typeof AboutRoute;
-  "/gossip": typeof GossipRoute;
-  "/leaderSchedule": typeof LeaderScheduleRoute;
-  "/slotDetails": typeof SlotDetailsRoute;
+  '/': typeof IndexRoute
+  '/about': typeof AboutRoute
+  '/gossip': typeof GossipRoute
+  '/leaderSchedule': typeof LeaderScheduleRoute
+  '/slotDetails': typeof SlotDetailsRoute
 }
-
 export interface FileRoutesById {
-  __root__: typeof rootRoute;
-  "/": typeof IndexRoute;
-  "/about": typeof AboutRoute;
-  "/gossip": typeof GossipRoute;
-  "/leaderSchedule": typeof LeaderScheduleRoute;
-  "/slotDetails": typeof SlotDetailsRoute;
+  __root__: typeof rootRouteImport
+  '/': typeof IndexRoute
+  '/about': typeof AboutRoute
+  '/gossip': typeof GossipRoute
+  '/leaderSchedule': typeof LeaderScheduleRoute
+  '/slotDetails': typeof SlotDetailsRoute
 }
-
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath;
-  fullPaths: "/" | "/about" | "/gossip" | "/leaderSchedule" | "/slotDetails";
-  fileRoutesByTo: FileRoutesByTo;
-  to: "/" | "/about" | "/gossip" | "/leaderSchedule" | "/slotDetails";
+  fileRoutesByFullPath: FileRoutesByFullPath
+  fullPaths: '/' | '/about' | '/gossip' | '/leaderSchedule' | '/slotDetails'
+  fileRoutesByTo: FileRoutesByTo
+  to: '/' | '/about' | '/gossip' | '/leaderSchedule' | '/slotDetails'
   id:
-    | "__root__"
-    | "/"
-    | "/about"
-    | "/gossip"
-    | "/leaderSchedule"
-    | "/slotDetails";
-  fileRoutesById: FileRoutesById;
+    | '__root__'
+    | '/'
+    | '/about'
+    | '/gossip'
+    | '/leaderSchedule'
+    | '/slotDetails'
+  fileRoutesById: FileRoutesById
+}
+export interface RootRouteChildren {
+  IndexRoute: typeof IndexRoute
+  AboutRoute: typeof AboutRoute
+  GossipRoute: typeof GossipRoute
+  LeaderScheduleRoute: typeof LeaderScheduleRoute
+  SlotDetailsRoute: typeof SlotDetailsRoute
 }
 
-export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute;
-  AboutRoute: typeof AboutRoute;
-  GossipRoute: typeof GossipRoute;
-  LeaderScheduleRoute: typeof LeaderScheduleRoute;
-  SlotDetailsRoute: typeof SlotDetailsRoute;
+declare module '@tanstack/react-router' {
+  interface FileRoutesByPath {
+    '/slotDetails': {
+      id: '/slotDetails'
+      path: '/slotDetails'
+      fullPath: '/slotDetails'
+      preLoaderRoute: typeof SlotDetailsRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/leaderSchedule': {
+      id: '/leaderSchedule'
+      path: '/leaderSchedule'
+      fullPath: '/leaderSchedule'
+      preLoaderRoute: typeof LeaderScheduleRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/gossip': {
+      id: '/gossip'
+      path: '/gossip'
+      fullPath: '/gossip'
+      preLoaderRoute: typeof GossipRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/about': {
+      id: '/about'
+      path: '/about'
+      fullPath: '/about'
+      preLoaderRoute: typeof AboutRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+  }
 }
 
 const rootRouteChildren: RootRouteChildren = {
@@ -147,40 +131,7 @@ const rootRouteChildren: RootRouteChildren = {
   GossipRoute: GossipRoute,
   LeaderScheduleRoute: LeaderScheduleRoute,
   SlotDetailsRoute: SlotDetailsRoute,
-};
-
-export const routeTree = rootRoute
-  ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>();
-
-/* ROUTE_MANIFEST_START
-{
-  "routes": {
-    "__root__": {
-      "filePath": "__root.tsx",
-      "children": [
-        "/",
-        "/about",
-        "/gossip",
-        "/leaderSchedule",
-        "/slotDetails"
-      ]
-    },
-    "/": {
-      "filePath": "index.tsx"
-    },
-    "/about": {
-      "filePath": "about.tsx"
-    },
-    "/gossip": {
-      "filePath": "gossip.tsx"
-    },
-    "/leaderSchedule": {
-      "filePath": "leaderSchedule.tsx"
-    },
-    "/slotDetails": {
-      "filePath": "slotDetails.tsx"
-    }
-  }
 }
-ROUTE_MANIFEST_END */
+export const routeTree = rootRouteImport
+  ._addFileChildren(rootRouteChildren)
+  ._addFileTypes<FileRouteTypes>()
